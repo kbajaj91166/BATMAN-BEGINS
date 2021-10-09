98fada972e0b84f6267d4a0b3c6380d5908c7bcf
kbajaj91166 <82516951+kbajaj91166@users.noreply.github.com>
2021-10-09 19:13:23 +0530

    Add files via upload

---
+++ PROC41-Template-master/Drop.js
@@ -0,0 +1,21 @@
+class Drop{
+    constructor(x,y){
+        var options = {
+            friction: 0.001,
+            restitution:0.1           
+        }
+        this.drop=Bodies.circle(x,y,5,options);
+        this.r=5;
+        World.add(world,this.drop);
+    }
+    update(){
+        if(this.drop.position.y>height){
+            Matter.Body.setPosition(this.drop,{x:random(0,400),y:random(0,400)})
+        }
+    }
+    display(){
+       fill("blue")
+       ellipseMode(CENTER);
+       ellipse(this.drop.position.x,this.drop.position.y,this.r,this.r)
+    }
+}
